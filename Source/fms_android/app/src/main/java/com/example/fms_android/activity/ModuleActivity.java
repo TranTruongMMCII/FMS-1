0f5689c3b94696c9e51f5d5bd980e89aa6638301
TranTruongMMCII <truongtlt147@gmail.com>
2021-05-21 15:33:26 +0700

    --datepicker

--- Source/fms_android/app/src/main/java/com/example/fms_android/activity/ModuleActivity.java
+++ Source/fms_android/app/src/main/java/com/example/fms_android/activity/ModuleActivity.java
@@ -1,8 +1,11 @@
 package com.example.fms_android.activity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
@@ -15,6 +18,8 @@ import com.example.fms_android.R;
 import com.example.fms_android.service.ModuleService;
 import com.example.fms_android.utility.APIUtility;
 
+import java.util.Calendar;
+
 public class ModuleActivity extends AppCompatActivity {
     private EditText txtModuleName;
     private TextView txtStartDate, txtEndDate, txtStartDateError, txtEndDateError,
@@ -26,6 +31,9 @@ public class ModuleActivity extends AppCompatActivity {
     private Button btnSave, btnCancel;
     private Intent intent;
     private ModuleService moduleService;
+    private int lastSelectedYear;
+    private int lastSelectedMonth;
+    private int lastSelectedDayOfMonth;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,6 +43,11 @@ public class ModuleActivity extends AppCompatActivity {
 
         moduleService = APIUtility.getModuleService();
         intent = getIntent();
+
+        final Calendar c = Calendar.getInstance();
+        this.lastSelectedYear = c.get(Calendar.YEAR);
+        this.lastSelectedMonth = c.get(Calendar.MONTH);
+        this.lastSelectedDayOfMonth = c.get(Calendar.DAY_OF_MONTH);
 
         initViews();
     }
@@ -57,5 +70,51 @@ public class ModuleActivity extends AppCompatActivity {
         spinnerFeedbackTitle = findViewById(R.id.spinnerFeedbackTitle);
         btnSave = findViewById(R.id.btnSave);
         btnCancel = findViewById(R.id.btnCancel);
+
+        imgEndDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+
+                        txtEndDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                    }
+                };
+
+                DatePickerDialog datePickerDialog = null;
+
+                datePickerDialog = new DatePickerDialog(ModuleActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_NoActionBar,
+                        dateSetListener, lastSelectedYear, lastSelectedMonth, lastSelectedDayOfMonth);
+                datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis() - 1000);
+                datePickerDialog.show();
+            }
+        });
+
+        imgStartDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+
+                        txtStartDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                    }
+                };
+
+                DatePickerDialog datePickerDialog = null;
+
+                datePickerDialog = new DatePickerDialog(ModuleActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_NoActionBar,
+                        dateSetListener, lastSelectedYear, lastSelectedMonth, lastSelectedDayOfMonth);
+                datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis() - 1000);
+                datePickerDialog.show();
+            }
+        });
     }
 }
